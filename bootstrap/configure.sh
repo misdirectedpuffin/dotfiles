bacc042ab902a87f6b90c794ac9039630b46dc91
misdirectedpuffin <misdirectedpuffin@gmail.com>
2019-08-23 16:05:58 +0100

    Tidy up after initial run

--- bootstrap/configure.sh
+++ bootstrap/configure.sh
@@ -3,6 +3,9 @@
 # Install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Python packages
+pip install poetry virtualenv virtualenvwrapper
+
 echo "Updating package lists..."
 brew update
 
@@ -13,12 +16,13 @@ echo "Installing drone cli"
 brew tap drone/drone
 brew install drone
 
+echo "Installing java..."
+brew cask install adoptopenjdk
+
 echo "Installing packages..."
 brew install ack \
     autoconf \
     awscli \
-    cairo \
-    certbot \
     cfssl \
     cloc \
     ctags \
@@ -29,7 +33,6 @@ brew install ack \
     gcc \
     gdbm \
     gettext \
-    giflib \
     git \
     bash-completion \
     glib \
@@ -37,36 +40,28 @@ brew install ack \
     gnupg \
     gnutls \
     go \
-    graphite2 \
-    harfbuzz \
     htop \
     jq \
     kubectx \
     kubernetes-cli \
     kubernetes-helm \
-    leiningen \
     libevent \
     libffi \
-    libgcrypt \
     libgpg-error \
-    little-cms2 \
-    maven \
     mitmproxy \
     ncurses \
     nmap \
     node \
     openssl \
-    protobuf \
     pyenv \
     pyenv-virtualenv \
     pyenv-virtualenvwrapper \
     python \
     readline \
-    ruby \
+    speedtest-cli \
     sqlite \
     stern \
     syncthing \
-    terraform \
     tmux \
     tree \
     vim \
@@ -75,16 +70,6 @@ brew install ack \
     zsh-completions
 
 
-echo ''
-echo "Now configuring git-completion..."
-GIT_VERSION=`git --version | awk '{print $3}'`
-URL="https://raw.github.com/git/git/v$GIT_VERSION/contrib/completion/git-completion.bash"
-echo ''
-echo "Downloading git-completion for git version: $GIT_VERSION..."
-if ! curl "$URL" --silent --output "$HOME/.git-completion.bash"; then
-	echo "ERROR: Couldn't download completion script. Make sure you have a working internet connection." && exit 1
-fi
-
 # oh-my-zsh install
 echo ''
 echo "Now installing oh-my-zsh..."
@@ -92,12 +77,14 @@ echo ''
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # oh-my-zsh plugin install
-echo ''
-echo "Now installing oh-my-zsh plugins..."
-echo ''
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+mkdir $ZSH/plugins/poetry
+poetry completions zsh > $ZSH/plugins/poetry/_poetry
+# echo ''
+# echo "Now installing oh-my-zsh plugins..."
+# echo ''
+# git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+# git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 # powerlevel9k install
 echo ''
@@ -111,23 +98,15 @@ echo "Now installing vundle..."
 echo ''
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Speedtest-cli and jq install
-brew install jq speedtest-cli
-
 # Pull down personal dotfiles
 echo ''
 echo "Now pulling down your dotfiles..."
-git clone https://github.com/misdirectedpuffin/dotfiles.git ~/.dotfiles
+git clone https://github.com/$USER/dotfiles.git ~/.dotfiles
 echo ''
 cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
 echo 'Syncing dotfiles with bonclay...'
 bonclay sync bonclay.conf.yaml
-then
-    echo "Successfully configured your environment with dotfiles..."
-else
-    echo "dotfiles were not applied successfully..." >&2
-# Set default shell to zsh
-echo ''
+echo "Successfully configured your environment with dotfiles..."
 
 echo "Now setting default shell..."
 chsh -s $(which zsh); exit 0
@@ -136,5 +115,9 @@ if [[ $? -eq 0 ]] then
 else
     echo "Default shell not set successfully..." >&2
 fi
-echo ''
+
+
+echo 'Installing Vundle plugins...'
+vim +PluginInstall +qall
+. ~/.zshrc
 echo '	Badass macOS terminal installed!'
